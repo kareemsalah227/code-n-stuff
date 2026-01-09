740b63dab21b1954b5799378a6c53784d5722c1d
Kareem Salah <kareem@Kareems-MacBook-Air.local>
2026-01-09 18:24:30 +0100

    Uses gemini models

--- scripts/flashcards-helper.py
+++ scripts/flashcards-helper.py
@@ -1,20 +1,31 @@
 import requests
 import json
+from google import genai
+from google.genai import types
 
 ANKI_CONNECT_URL = "http://localhost:8765"
-OLLAMA_URL = "http://localhost:11434/api/generate"
-MODEL = "deepseek-r1:8b"
-# MODEL = "llama3"
+
+client = genai.Client()
 
 def get_word_details(word):
+    # prompt = f"""
+    # You are a German linguistics expert. Analyze the word: "{word}", and return 3 JSON fields, namely "translation", "line1" and "line2".
+    
+    # 1. For the field "translation", provide the most common English or Arabic translation along side the definite article. Example: "the table" or "الطاولة".
+    
+    # 2. For the field "line1", If the word is a noun, then find the article for this word and the plural form (e.g., "der Tisch, die Tische"). If the word is a verb, an adjective or a preposition, then just the word itself.
+    
+    # 3. For the field "line2", If the word is a verb, then provide the indicative (Präsens, Präteritum, Perfekt), the konjunktiv (Konjunktiv I, Konjunktiv II Präsens, Konjunktiv II Perfekt) and a small example sentence. If the word is an adjective, then provide the Comparative and superlative forms, along with a small example sentence. If the word is a noun or a preposition, then provide only a small example sentence.
+    # """
+
     prompt = f"""
     You are a German linguistics expert. Analyze the word: "{word}".
 
     Return ONLY a JSON object:
     {{
       "translation": "An English or Arabic of the word according to the rules below",
-      "line1": "The German word, with additional info as per the rules below",
-      "line2": "Additional details in German as per the rules below"
+      "line1": "The word, with additional info as per the rules below",
+      "line2": "Additional details as per the rules below"
     }}
     
     RULES:
@@ -28,23 +39,29 @@ def get_word_details(word):
     
     3. Field "line2":
        - If the word is a verb, then provide exactly 3 lines:
-          - Indicative (Präsens, Präteritum, Perfekt)
-          - Konjunktiv (Konjunktiv I, Konjunktiv II Präsens, Konjunktiv II Perfekt)
-          - Small example sentence
+          - Indicative verb stem without ending (Präsens, Präteritum, Perfekt)
+          - Konjunktiv verb stem without ending (Konjunktiv I, Konjunktiv II Präsens, Konjunktiv II Perfekt)
+          - Example Sentence
        - If the word is an adjective, then provide 2 lines:
          - The Comparative, Superlative forms. Example: "schneller, am schnellsten".
-         - Small example sentence
-       - If the word is a noun or a preposition, then return only a small example sentence.
+         - An Example Sentence
+       - If the word is a noun or a preposition: Example Sentence only
     """
     
     try:
-        response = requests.post(OLLAMA_URL, json={
-            "model": MODEL, "prompt": prompt, "stream": False, "format": "json"
-        }, timeout=None)
-        print(f"Raw response: {response.text}")  # Debugging line
-        return json.loads(response.json()['response'])
+        response = client.models.generate_content(
+            # model='gemini-2.5-flash',
+            model="gemini-3-flash-preview",
+            contents=prompt,
+            config=types.GenerateContentConfig(
+                response_mime_type='application/json',
+            )
+        )
+        non_raw_response = json.loads(response.text)
+        return non_raw_response
     except Exception as e:
         print(f"Error: {e}")
+        exit(1)
         return None
 
 def add_to_anki(data):
